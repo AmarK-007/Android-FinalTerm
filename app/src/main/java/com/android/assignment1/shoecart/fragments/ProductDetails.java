ac6e6d4fc3fcaadd6dde693b0364296119f39eef
Deptl <pateldeep312000@gmail.com>
2024-04-14 22:03:27 -0400

    Product Details Page

---
+++ app/src/main/java/com/android/assignment1/shoecart/fragments/ProductDetails.java
@@ -0,0 +1,47 @@
+package com.android.assignment1.shoecart.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.android.assignment1.shoecart.R;
+
+public class ProductDetails extends Fragment {
+
+    private SeekBar seekBar;
+    private TextView valueTextView;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_product_details, container, false);
+
+        seekBar = view.findViewById(R.id.shoeSizeSeekBar);
+        valueTextView = view.findViewById(R.id.shoeSize);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int value = progress;
+                valueTextView.setText(String.valueOf(value));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        return view;
+    }
+}
